e243f8f211fc3106cb249207adabb33028afc7f6
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-08 23:01:47 -0300

    feat: added context providers in App

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
-import { render } from 'react-dom'
+// eslint-disable-next-line import/no-unresolved
+import { render } from 'react-dom';
 
-import { App } from './App'
+import { App } from './App';
+import { GenresContextProvider } from './contexts/GenresContext';
+import { MovieContextProvider } from './contexts/MovieContext';
 
-render(<App />, document.getElementById('root'))
+render(
+  <GenresContextProvider>
+    <MovieContextProvider>
+      <App />
+    </MovieContextProvider>
+  </GenresContextProvider>,
+  document.getElementById('root'),
+);
