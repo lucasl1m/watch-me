ce9b8efa0c131bb3b0d6b8848d1166c0ad7deb5a
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-08 23:04:39 -0300

    feat: structuring component Sidebar & Content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
-  // Complete aqui
-}
+import { useContext, useEffect } from 'react';
+
+import GenresContext, { GenreResponseProps } from '../contexts/GenresContext';
+import { api } from '../services/api';
+import '../styles/sidebar.scss';
+import { Button } from './common/button/button';
+
+export const SideBar = () => {
+  const { genres, setGenres, selectedGenreId, setSelectedGenreId } = useContext(GenresContext);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data);
+    });
+  });
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+};
